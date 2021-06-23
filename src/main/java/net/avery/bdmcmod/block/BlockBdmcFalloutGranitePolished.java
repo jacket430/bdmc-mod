f9ce9d64f3b44fe95f8a57213b23250ee35fed97
jacket430 <balls@DESKTOP-HJH6CQB>
2021-06-23 17:35:22 -0600

    First batch of blocks and recipes

--- src/main/java/net/avery/bdmcmod/block/BlockBdmcFalloutGranitePolished.java
+++ src/main/java/net/avery/bdmcmod/block/BlockBdmcFalloutGranitePolished.java
@@ -7,14 +7,9 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 
-import net.minecraft.world.IBlockAccess;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.NonNullList;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.Item;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.Block;
@@ -23,16 +18,16 @@ import net.avery.bdmcmod.creativetab.TabBdmcBlocks;
 import net.avery.bdmcmod.ElementsBdmcModMod;
 
 @ElementsBdmcModMod.ModElement.Tag
-public class BlockBdmcFalloutStone extends ElementsBdmcModMod.ModElement {
-	@GameRegistry.ObjectHolder("bdmc_mod:bdmc_fallout_stone")
+public class BlockBdmcFalloutGranitePolished extends ElementsBdmcModMod.ModElement {
+	@GameRegistry.ObjectHolder("bdmc_mod:bdmc_fallout_granite_polished")
 	public static final Block block = null;
-	public BlockBdmcFalloutStone(ElementsBdmcModMod instance) {
-		super(instance, 1);
+	public BlockBdmcFalloutGranitePolished(ElementsBdmcModMod instance) {
+		super(instance, 6);
 	}
 
 	@Override
 	public void initElements() {
-		elements.blocks.add(() -> new BlockCustom().setRegistryName("bdmc_fallout_stone"));
+		elements.blocks.add(() -> new BlockCustom().setRegistryName("bdmc_fallout_granite_polished"));
 		elements.items.add(() -> new ItemBlock(block).setRegistryName(block.getRegistryName()));
 	}
 
@@ -40,12 +35,12 @@ public class BlockBdmcFalloutStone extends ElementsBdmcModMod.ModElement {
 	@Override
 	public void registerModels(ModelRegistryEvent event) {
 		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0,
-				new ModelResourceLocation("bdmc_mod:bdmc_fallout_stone", "inventory"));
+				new ModelResourceLocation("bdmc_mod:bdmc_fallout_granite_polished", "inventory"));
 	}
 	public static class BlockCustom extends Block {
 		public BlockCustom() {
 			super(Material.ROCK);
-			setUnlocalizedName("bdmc_fallout_stone");
+			setUnlocalizedName("bdmc_fallout_granite_polished");
 			setSoundType(SoundType.STONE);
 			setHarvestLevel("pickaxe", 1);
 			setHardness(1.5F);
@@ -53,11 +48,6 @@ public class BlockBdmcFalloutStone extends ElementsBdmcModMod.ModElement {
 			setLightLevel(0F);
 			setLightOpacity(255);
 			setCreativeTab(TabBdmcBlocks.tab);
-		}
-
-		@Override
-		public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
-			drops.add(new ItemStack(BlockBdmcFalloutCobblestone.block, (int) (1)));
 		}
 	}
 }
