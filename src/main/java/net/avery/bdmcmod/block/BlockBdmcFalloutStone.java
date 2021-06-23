862cad5fe585e2621a252e6629e02cdcbc36ff0f
jacket430 <balls@DESKTOP-HJH6CQB>
2021-06-23 05:51:09 -0600

    Initial commit, 3 blocks.

---
+++ src/main/java/net/avery/bdmcmod/block/BlockBdmcFalloutStone.java
@@ -0,0 +1,62 @@
+
+package net.avery.bdmcmod.block;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.NonNullList;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.Item;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.Block;
+
+import net.avery.bdmcmod.creativetab.TabBdmcBlocks;
+import net.avery.bdmcmod.ElementsBdmcModMod;
+
+@ElementsBdmcModMod.ModElement.Tag
+public class BlockBdmcFalloutStone extends ElementsBdmcModMod.ModElement {
+	@GameRegistry.ObjectHolder("bdmc_mod:bdmc_fallout_stone")
+	public static final Block block = null;
+	public BlockBdmcFalloutStone(ElementsBdmcModMod instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new BlockCustom().setRegistryName("bdmc_fallout_stone"));
+		elements.items.add(() -> new ItemBlock(block).setRegistryName(block.getRegistryName()));
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0,
+				new ModelResourceLocation("bdmc_mod:bdmc_fallout_stone", "inventory"));
+	}
+	public static class BlockCustom extends Block {
+		public BlockCustom() {
+			super(Material.ROCK);
+			setUnlocalizedName("bdmc_fallout_stone");
+			setSoundType(SoundType.STONE);
+			setHardness(1.5F);
+			setResistance(10F);
+			setLightLevel(0F);
+			setLightOpacity(255);
+			setCreativeTab(TabBdmcBlocks.tab);
+		}
+
+		@Override
+		public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+			drops.add(new ItemStack(BlockBdmcFalloutCobblestone.block, (int) (1)));
+		}
+	}
+}
